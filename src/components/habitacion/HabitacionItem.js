b360ea06c82e4b502e27ab4519a9a511e3ae3a35
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-07-03 09:39:18 -0500

    agrega switch case para renderizar estilos dependiendo el estado

--- src/components/habitacion/HabitacionItem.js
+++ src/components/habitacion/HabitacionItem.js
@@ -1,4 +1,5 @@
 import { formatCurrency } from "../../utils/utils";
+import PopupReserva from "../reserva/PopupReserva";
 
 export default class HabitacionItem {
   constructor(data) {
@@ -18,26 +19,58 @@ export default class HabitacionItem {
     return newCard;
   }
 
-
   _setEventListeners() {
     // Agregar el evento para el botón de editar
-    this._element
-      .addEventListener("click", () => {
-        console.log(this._habitacionId)
-      });
+    this._element.addEventListener("click", () => {
+      const popupReserva = new PopupReserva();
+      const popup = popupReserva.generatePopup();
+      popup.querySelector("#habitacion").value = this._numero;
+      popupReserva.open();
+      document.querySelector(".popupReserva").prepend(popup);
+    });
     // Agregar el evento para el botón de eliminar
-   
   }
 
   generateRoom() {
     this._element = this._getTemplate();
     this._setEventListeners();
     this._element.id = this._habitacionId;
-    this._element.querySelector(".room__card-number").textContent = this._numero;
-    this._element.querySelector(".room__card-category").textContent = this._categoria;
-    this._element.querySelector(".room__card-price").textContent = formatCurrency(this._precio);
-    this._element.querySelector(".room__card-status").textContent = this._estado.toUpperCase();
+    this._element.querySelector(".room__card-number").textContent =
+      this._numero;
+    this._element.querySelector(".room__card-category").textContent =
+      this._categoria;
+    this._element.querySelector(".room__card-price").textContent =
+      formatCurrency(this._precio);
+    this._element.querySelector(".room__card-status").textContent =
+      this._estado.toUpperCase();
+    this._element.querySelector(".room-status__icon").alt = this._estado;
 
+     switch (this._estado) {
+        case "Disponible":
+          this._element.classList.add("border-lime-600");
+          this._element.querySelector(".room__card-status").classList.add("bg-lime-600");
+          this._element.querySelector(".room-status__icon").classList.add("bg-lime-600");
+          this._element.querySelector(".room-status__icon").src = "./src/images/disponible.png";
+          break;
+        case "Ocupada":
+          this._element.classList.add("border-amber-600");
+          this._element.querySelector(".room__card-status").classList.add("bg-amber-600");
+          this._element.querySelector(".room-status__icon").classList.add("bg-amber-600");
+          this._element.querySelector(".room-status__icon").src = "./src/images/ocupada.png";
+          break;
+        case "En Limpieza":
+          this._element.classList.add("border-blue-700");
+          this._element.querySelector(".room__card-status").classList.add("bg-blue-700");
+          this._element.querySelector(".room-status__icon").classList.add("bg-blue-600");
+          this._element.querySelector(".room-status__icon").src = "./src/images/limpieza.png";
+          break;
+        case "No Disponible":
+          this._element.classList.add("border-red-600");
+          this._element.querySelector(".room__card-status").classList.add("bg-red-600");
+          this._element.querySelector(".room-status__icon").classList.add("bg-red-600");
+          this._element.querySelector(".room-status__icon").src = "./src/images/no-disponible.png";
+          break;
+      }
 
     return this._element;
   }
--- src/components/reserva/ReservaItem.js
+++ src/components/reserva/ReservaItem.js
@@ -1,3 +1,5 @@
+import { formatDateTime } from "../../utils/utils";
+
 export default class ReservaItem {
   constructor(data) {
     this._reserva_id = data.id_reserva;
@@ -41,11 +43,39 @@ export default class ReservaItem {
     this._setEventListeners();
     this._element.id = this._reserva_id;
     this._element.querySelector(".reservation__room-number").textContent = this._numero_habitacion;
-    this._element.querySelector(".reservation__date").textContent = this._fecha_registro;
+    this._element.querySelector(".reservation__date").textContent = formatDateTime(this._fecha_registro);
     this._element.querySelector(".reservation__client-cedula").textContent = this._cedula;
     this._element.querySelector(".reservation__client-name").textContent = `${this._nombre_cliente} ${this._apellido_cliente}`;
     this._element.querySelector(".reservation__status").textContent = this._estado_reserva.toUpperCase();
+    this._element.querySelector(".reservation__icon").alt = this._estado_reserva;
 
+
+    switch (this._estado_reserva) {
+        case "Pendiente":
+          this._element.classList.add("border-gray-500");
+          this._element.querySelector(".reservation__status").classList.add("bg-gray-500");
+          this._element.querySelector(".reservation__icon").classList.add("bg-gray-500");
+          this._element.querySelector(".reservation__icon").src = "./src/images/pendiente.png";
+          break;
+        case "Activa":
+          this._element.classList.add("border-blue-700");
+          this._element.querySelector(".reservation__status").classList.add("bg-blue-700");
+          this._element.querySelector(".reservation__icon").classList.add("bg-blue-700");
+          this._element.querySelector(".reservation__icon").src = "./src/images/activa.png";
+            break;
+        case "Completada":
+          this._element.classList.add("border-lime-600");
+          this._element.querySelector(".reservation__status").classList.add("bg-lime-600");
+          this._element.querySelector(".reservation__icon").classList.add("bg-lime-600");
+          this._element.querySelector(".reservation__icon").src = "./src/images/completada.png";
+          break;
+        case "Cancelada":
+          this._element.classList.add("border-red-700");
+          this._element.querySelector(".reservation__status").classList.add("bg-red-700");
+          this._element.querySelector(".reservation__icon").classList.add("bg-red-700");
+          this._element.querySelector(".reservation__icon").src = "./src/images/no-disponible.png";
+          break;
+      }
 
     return this._element;
   }
