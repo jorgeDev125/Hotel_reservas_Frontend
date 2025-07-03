9cf0b4d7af9a45a373a14c6f7ee9109eab587037
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-07-03 09:36:42 -0500

    agrega funcion formtaDateTime a util.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,3 +5,14 @@ export function formatCurrency (amount) {
     }).format(amount)
 }
 
+export function formatDateTime(fechaISO) {
+  const date = new Date(fechaISO);
+  return date.toLocaleString('es-CO', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+  });
+}
