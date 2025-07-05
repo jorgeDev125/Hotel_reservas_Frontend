6e4116aa589815126e342a9954eab56a2c25225e
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-07-05 07:04:11 -0500

    Agrega PopupReserva completo

--- src/components/reserva/PopupReserva.js
+++ src/components/reserva/PopupReserva.js
@@ -1,4 +1,8 @@
+import { apiClienteInstance } from "../../utils/clientes";
+import { apiHabitacionInstance } from "../../utils/habitacion";
 import { apiReservaInstance } from "../../utils/reserva";
+import { formatCurrency } from "../../utils/utils";
+import PopupCliente from "../cliente/PopupCliente";
 import ReservaItem from "./ReservaItem";
 
 export default class PopupReserva {
@@ -15,12 +19,36 @@ export default class PopupReserva {
   generatePopup() {
     this._popup = this._getTemplate();
     this._setEventListeners();
+
+    try {
+      const select = this._popup.querySelector("#selectHabitacion");
+      apiHabitacionInstance
+        .buscarHabitacionesPorEstado(1)
+        .then((habitaciones) => {
+          habitaciones.forEach((h) => {
+            const option = document.createElement("option");
+            option.value = h.habitacion_id;
+            option.textContent = `Hab. ${h.numero}`;
+            option.setAttribute("data-numero", h.numero);
+            option.setAttribute("data-categoria", h.categoria);
+            option.setAttribute("data-precio", h.precio);
+            select.appendChild(option);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {}
+
     return this._popup;
   }
 
   open() {
     document.querySelector(".popupReserva").classList.remove("hidden");
-    document.querySelector(".popupReserva").classList.add("flex");
+    document.querySelector(".popupReserva").classList.add("flex", "top-5");
+    document.querySelector(
+      ".popupReserva"
+    ).style = `left: calc((100vw - 448px) / 2);`;
     document.querySelector(".overlay").classList.remove("hidden");
     // Autoenfocar el input de cédula cuando se abre el modal
     setTimeout(() => {
@@ -38,6 +66,99 @@ export default class PopupReserva {
   }
 
   _setEventListeners() {
+    //agrega el evento para buscar cliente por cédula
+    this._popup
+      .querySelector("#buscarClienteButton")
+      .addEventListener("click", (evt) => {
+        evt.preventDefault();
+        const formData = new FormData(
+          this._popup.querySelector("#reservaForm")
+        );
+
+        if (!formData.get("cedula")) {
+          alert("Ingresa un número de cédula");
+          return;
+        }
+        //obtiene los datos de la base de datos y los renderiza
+        apiClienteInstance
+          .obtenerClientePorCedula(formData.get("cedula"))
+          .then((cliente) => {
+            this._popup.querySelector("#cliente_cedula").textContent =
+              cliente[0].cedula;
+            this._popup.querySelector(
+              "#cliente_nombre"
+            ).textContent = `${cliente[0].nombre} ${cliente[0].apellido}`;
+            this._popup.querySelector("#cliente_telefono").textContent =
+              cliente[0].telefono;
+            this._popup.querySelector("#cliente_email").textContent =
+              cliente[0].email;
+            this._popup.querySelector("#cliente_id").value = cliente[0].id;
+          })
+          .catch((error) => {
+
+            if (window.confirm(error + "¿Quieres crear el usuario?")) {
+              
+              this.close()
+              localStorage.setItem("cedula_para_popup", formData.get("cedula"));
+              // Redirige a /clientes
+              window.location.href = "/clientes";
+              
+            } 
+          });
+      });
+
+    //evento para seleccionar habitación y renderizar
+    this._popup
+      .querySelector("#selectHabitacion")
+      .addEventListener("change", (event) => {
+        const select = event.target;
+        const selected = select.options[select.selectedIndex];
+        const numero = selected.getAttribute("data-numero") || "_____";
+        const categoria =
+          selected.getAttribute("data-categoria") || "__________";
+        const precio = selected.getAttribute("data-precio") || "_______";
+
+        // Actualiza el resumen
+        this._popup.querySelector("#habitacion_numero").textContent = numero;
+        this._popup.querySelector("#habitacion_categoria").textContent =
+          categoria;
+        this._popup.querySelector("#habitacion_precio").textContent =
+          formatCurrency(precio);
+        this._popup.querySelector("#habitacion_id").value = selected.value;
+      });
+
+    //evento para el select de las noches
+    this._popup
+      .querySelector("#numero_noches")
+      .addEventListener("change", (event) => {
+        const selected = event.target.value;
+
+        // Actualiza el resumen
+        this._popup.querySelector("#resumen_noches").value = selected;
+      
+      });
+
+    //evento para calcular precio total de reservación
+    const selectHabitacion = this._popup.querySelector("#selectHabitacion");
+    const inputNoches = this._popup.querySelector("#numero_noches");
+    const spanPrecioTotal = this._popup.querySelector("#precio_total");
+
+    function calcularPrecioTotal() {
+      // Obtiene la opción seleccionada
+      const selected = selectHabitacion.options[selectHabitacion.selectedIndex];
+      const precioHabitacion = Number(selected.getAttribute("data-precio")) || 0;
+      const numeroNoches = Number(inputNoches.value) || 0;
+      const precioTotal = formatCurrency(precioHabitacion * numeroNoches);
+
+      // Formatea el precio en COP sin decimales
+      spanPrecioTotal.textContent = precioTotal;
+    }
+
+    // Escucha cambios en ambos campos
+    selectHabitacion.addEventListener("input", calcularPrecioTotal);
+    inputNoches.addEventListener("input", calcularPrecioTotal);
+
+
     // Agregar el evento para el botón de enviar del formulario
     this._popup
       .querySelector("#reservaForm")
@@ -47,9 +168,9 @@ export default class PopupReserva {
           this._popup.querySelector("#reservaForm")
         );
         const reservaData = {
-          clienteId: formData.get("cedula"),
-          numeroNoches: formData.get("noches"),
-          habitacionId: formData.get("habitacion"),
+          clienteId: formData.get("cliente_id"),
+          numeroNoches: formData.get("resumen_noches"),
+          habitacionId: formData.get("habitacion_id"),
         };
 
         // reactualizar la lista de reservas
@@ -57,7 +178,7 @@ export default class PopupReserva {
         apiReservaInstance
           .crearReservacion(reservaData)
           .then(() => {
-            reservasLista.innerHTML = ""; // Limpiar la lista actual
+            reservasLista.textContent = ""; // Limpiar la lista actual
 
             // Volver a cargar los reservas
             return apiReservaInstance.obtenerReservaciones();
@@ -69,6 +190,8 @@ export default class PopupReserva {
                   new ReservaItem(reserva).generateReserva()
                 );
               });
+              this.close(); // Cerrar el popup después de crear el reserva
+              alert("Reserva creada exitosamente");
             } else {
               const p = document.createElement("p");
               p.classList.add(
@@ -81,8 +204,7 @@ export default class PopupReserva {
               p.textContent = "No hay reservacioness disponibles.";
               reservasLista.appendChild(p);
             }
-            this.close(); // Cerrar el popup después de crear el reserva
-            alert("Reserva creada exitosamente");
+            
           })
           .catch((error) => {
             alert(error);
--- src/utils/reserva.js
+++ src/utils/reserva.js
@@ -1,6 +1,8 @@
 import { ApiReserva } from "../components/reserva/ApiReserva";
+import PopupReserva from "../components/reserva/PopupReserva";
 import ReservaItem from "../components/reserva/ReservaItem";
 import ReservaView from "../components/reserva/ReservaView";
+import { apiClienteInstance } from "./clientes";
 import { apiInstanceConfig } from "./constants";
 
 //crea instancia de la clase ApiReserva
@@ -10,6 +12,50 @@ export const apiReservaInstance = new ApiReserva(apiInstanceConfig);
 export async function renderReservaView() {
   const reservaVista = new ReservaView().generateView();
   const reservaLista = reservaVista.querySelector("#reservasList");
+
+  const cedula = localStorage.getItem("cedula_para_popup");
+    if (cedula) {
+      // Aquí tu lógica para abrir el popup y poner la cédula
+      const popupReserva = new PopupReserva();
+      const popup = popupReserva.generatePopup();
+      popup.querySelector("#cedula").value = cedula;
+      apiClienteInstance
+                .obtenerClientePorCedula(cedula)
+                .then((cliente) => {
+                  popup.querySelector("#cliente_cedula").textContent =
+                    cliente[0].cedula;
+                  popup.querySelector(
+                    "#cliente_nombre"
+                  ).textContent = `${cliente[0].nombre} ${cliente[0].apellido}`;
+                  popup.querySelector("#cliente_telefono").textContent =
+                    cliente[0].telefono;
+                  popup.querySelector("#cliente_email").textContent =
+                    cliente[0].email;
+                  popup.querySelector("#cliente_id").value = cliente[0].id;
+                })
+                .catch((error) => {console.log(error)})
+      popupReserva.open();
+      document.querySelector(".popupReserva").prepend(popup);
+  
+      // Limpia el valor para que no se repita la acción en futuros reloads
+      localStorage.removeItem("cedula_para_popup");
+    }
+
+    const habitacion = JSON.parse(localStorage.getItem("habitacion_para_popup"));
+      if (habitacion) {
+        // Aquí tu lógica para abrir el popup y poner la habitacion
+        console.log(habitacion)
+        const popupReserva = new PopupReserva();
+        const popup = popupReserva.generatePopup();
+        popup.querySelector("#habitacion_numero").textContent = habitacion.numero;
+        popup.querySelector("#habitacion_categoria").textContent = habitacion.categoria;
+        popup.querySelector("#habitacion_precio").textContent = habitacion.precio;
+        popupReserva.open();
+        document.querySelector(".popupReserva").prepend(popup);
+    
+        // Limpia el valor para que no se repita la acción en futuros reloads
+       localStorage.removeItem("habitacion_para_popup");
+  }
 
   let reservas = [];
   try {
